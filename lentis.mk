bdeccb13519f46196a62f62feceaaa87fe2aae05
underscoremone <underscoremone@gmail.com>
2017-08-29 17:58:29 +0100

    lentis-common: Move media configs to apq8084-common
    Change-Id: I0ecf682170dde627b9c50055ed6ad14b3cbb1eb2

--- lentis.mk
+++ lentis.mk
@@ -154,14 +154,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += qcmediaplayer
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
-
 # NFC
 PRODUCT_PACKAGES += \
     nfc_nci.pn54x.default \
